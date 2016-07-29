90b98c7329247bad50d03269a0512772a20a626e
hgd250 <hgd250@email.com>
2016-07-29 10:25:24 +0800

    scala

---
+++ src/Helloworld.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by guodong.hu on 7/27/2016.
+  */
+class Helloworld {
+
+
+}
+
+object Helloworld {
+
+  def main(args: Array[String]): Unit = {
+    println("hello world 0")
+    println("hello world 1")
+    println("hello world 2")
+  }
+}
